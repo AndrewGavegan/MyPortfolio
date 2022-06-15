9f66d453c49604c1ffd927959de923d21dd9a864
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-06-15 19:31:04 +1000

    changed JS links

--- assets/project.js
+++ assets/project.js
@@ -1,48 +1,48 @@
 
 // pop out menu for phone screens //
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 const navLink = document.querySelectorAll('.nav_link')
 
 // menu dissappears once a item is clicked //
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
-   
+
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // opens project in current tab and repository in new tab //
-document.querySelector("#project1").addEventListener('click', function(){
+document.querySelector("#project1").addEventListener('click', function () {
     window.open("https://github.com/JemimaSiddiqui/Project_1_Group-B")
     location.href = ("https://jemimasiddiqui.github.io/Project_1_Group-B/")
 });
 
 // opens project in current tab and repository in new tab //
-document.querySelector("#project2").addEventListener('click', function(){
-    window.open("https://github.com/AndrewGavegan/Weather-Dashboard")
-    location.href = ("https://andrewgavegan.github.io/Weather-Dashboard/")
+document.querySelector("#project2").addEventListener('click', function () {
+    window.open("https://github.com/AndrewGavegan/Popcornable")
+    location.href = ("https://popcornable.herokuapp.com/")
 });
 
 // opens project in current tab and repository in new tab //
-document.querySelector("#project3").addEventListener('click', function(){
-    window.open("https://github.com/AndrewGavegan/Work-Diary")
-    location.href = ("https://andrewgavegan.github.io/Work-Diary/")
+document.querySelector("#project3").addEventListener('click', function () {
+    window.open("https://github.com/AndrewGavegan/Technotizing")
+    location.href = ("https://technotizing.herokuapp.com/")
 });
 
 // adding copy to clipboard event for email button //
-document.getElementById("copyBtn").addEventListener('click', function(){
-    var email = document.getElementById("email") 
+document.getElementById("copyBtn").addEventListener('click', function () {
+    var email = document.getElementById("email")
 
     navigator.clipboard.writeText(email.innerText);
     alert("Copied Email")
